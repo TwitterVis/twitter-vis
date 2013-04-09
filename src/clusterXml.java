5845bbc223bd67eec0b20a9ac2ba8d37c960a303
Sean <Sean@Sean-HP>
2013-04-09 12:32:54 +0100

    Added inital gui element.

---
+++ src/clusterXml.java
@@ -0,0 +1,68 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import org.carrot2.clustering.stc.STCClusteringAlgorithm;
+import org.carrot2.core.Cluster;
+import org.carrot2.core.Controller;
+import org.carrot2.core.ControllerFactory;
+import org.carrot2.core.Document;
+import org.carrot2.core.ProcessingResult;
+import org.xml.sax.SAXException;
+
+public class clusterXml implements IResource {
+    
+    public clusterXml()
+    {
+     
+    }
+        public void clusterXmlFile(String url) throws IOException, ParserConfigurationException, SAXException
+        {
+            ReadXMLFile r = new ReadXMLFile();
+            String[][] data = r.returnTeewts(url);
+            System.out.println("Tweets converted");
+
+
+            /* Prepare Carrot2 documents */
+            final ArrayList<Document> documents = new ArrayList<Document>();
+            
+            for (String[] row : data)
+            {
+                documents.add(new Document(row[1], row[2], row[0]));
+            }
+            
+            System.out.println("tweets prepared");
+            
+            /* A controller to manage the processing pipeline. */
+            final Controller controller = ControllerFactory.createSimple();
+
+            /*
+             * Perform clustering by topic using the Lingo algorithm. Lingo can
+             * take advantage of the original query, so we provide it along with
+             * the documents.
+             */
+            final ProcessingResult byTopicClusters = controller.process(
+                    documents, "  ", STCClusteringAlgorithm.class);
+            final List<Cluster> clustersByTopic = byTopicClusters.getClusters();
+
+            /*
+             * Perform clustering by domain. In this case query is not useful,
+             * hence it is null.
+             */
+            System.out.println("clustering");
+//            final ProcessingResult byDomainClusters = controller.process(
+//            documents, null, ByUrlClusteringAlgorithm.class);
+//            final List<Cluster> clustersByDomain = byDomainClusters
+            //.getClusters();
+            // [[[end:clustering-document-list]]]
+
+            ConsoleFormatter.displayClusters(clustersByTopic);
+      
+           // ConsoleFormatter.displayClusters(clustersByDomain);
+            // return clustersByTopic;
+        }  
+        }
+
+
--- src/twitter_vis_gui.java
+++ src/twitter_vis_gui.java
@@ -1,12 +1,18 @@
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.xml.parsers.ParserConfigurationException;
+import org.carrot2.core.Cluster;
+import org.xml.sax.SAXException;
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Sean
@@ -37,10 +43,8 @@ public class twitter_vis_gui extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        clusterTextField.setText("jTextField1");
+        clusterTextField.setEditable(false);
         jScrollPane1.setViewportView(clusterTextField);
-
-        fileUrlTextField.setText("jTextField2");
 
         attachButton.setText("Attach");
         attachButton.addActionListener(new java.awt.event.ActionListener() {
@@ -66,11 +70,11 @@ public class twitter_vis_gui extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(fileUrlTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addComponent(fileUrlTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(attachButton))
                     .addComponent(RunButton))
-                .addContainerGap(37, Short.MAX_VALUE))
+                .addContainerGap(57, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -93,21 +97,38 @@ public class twitter_vis_gui extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void attachButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_attachButtonActionPerformed
-        JFileChooser fc= new JFileChooser();
-        fc.showOpenDialog(null);
-        File f= fc.getSelectedFile();
-        fileUrlTextField.setText(f.getAbsolutePath());
-        
+        try {
+            attach();
+        } catch (IOException ex) {
+            Logger.getLogger(twitter_vis_gui.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(twitter_vis_gui.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(twitter_vis_gui.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_attachButtonActionPerformed
 
-    private void RunButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RunButtonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_RunButtonActionPerformed
+    public void attach() throws IOException, ParserConfigurationException, SAXException {
+        JFileChooser fc = new JFileChooser();
+        fc.showOpenDialog(null);
+        File f = fc.getSelectedFile();
+         fileUrlTextField.setText(f.getAbsolutePath());  
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
+    private void RunButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RunButtonActionPerformed
+        String url =fileUrlTextField.getText();
+        clusterXml cluster = new clusterXml();
+         try {
+                cluster.clusterXmlFile(url);
+            
+        } catch (Exception e) {
+             System.out.println("failed to find file");
+        }
+    }//GEN-LAST:event_RunButtonActionPerformed
+/**
+ * @param args the command line arguments
+ */
+public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -118,16 +139,32 @@ public class twitter_vis_gui extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-                }
+                
+
+}
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(twitter_vis_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(twitter_vis_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(twitter_vis_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(twitter_vis_gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(twitter_vis_gui.class  
+
+.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } 
+
+catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(twitter_vis_gui.class  
+
+.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } 
+
+catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(twitter_vis_gui.class  
+
+.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } 
+
+catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(twitter_vis_gui.class  
+
+.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -138,6 +175,8 @@ public class twitter_vis_gui extends javax.swing.JFrame {
             }
         });
     }
+
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton RunButton;
     private javax.swing.JButton attachButton;
