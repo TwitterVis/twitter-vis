a76838b91609a5cfe541f754aa788a377f9e24ae
Ross <Ross@AsusK53>
2013-04-16 13:59:42 +0100

    Added chart examples. Added JFreeChart libraries.

---
+++ src/graph/MainTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+/**
+ *
+ * @author Ross
+ */
+public class MainTest {
+   public static void main(String[] args) {
+          PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
+          demo.pack();
+          demo.setVisible(true);
+      }
+} 
